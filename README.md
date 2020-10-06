db1dafa5f9f4c290bd1e565288395384cfd23c69
James Su-Shum <sushumjames@gmail.com>
2020-10-06 15:37:45 -0400

    Linked firebase, modified sql database, added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wisdom and Grace
+
+Project exercise using Firebase for user authentication and authorization.
+
+Wisdom and Grace is a full-stack app for managing Grace Hopper quotes using ASP.NET Core API on the server-side, Entity Framework Core to interact with a SQL Server database, React on the client-side and Firebase for authentication.
--- WisdomAndGrace.sql
+++ WisdomAndGrace.sql
@@ -54,8 +54,8 @@ SET IDENTITY_INSERT [UserProfile] ON
 INSERT INTO [UserProfile]
   ([Id], [FirebaseUserId], [Name], [Email], [UserTypeId])
 VALUES
-  (1, 'MA9qsgE6vfbp2P1z0kv72bqRql43', 'Foo Barington', 'foo@bar.com', 1),
-  (2, 'vP3tkzRXWmRzwSLGwNTBS5fJs2N2', 'Bar Bazaar', 'bar@baz.com', 2);
+  (1, 'Lb83x0YzZxYEobWgeCbYuQ7bDoe2', 'John Doe', 'john@me.com', 1),
+  (2, 'sUQzQnkGtjdVlKrakrnEfqXxHJP2', 'Jane Doe', 'jane@me.com', 2);
 SET IDENTITY_INSERT [UserProfile] OFF
 
 
